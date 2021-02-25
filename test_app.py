fa4a6923fa731a8d69e893e674c95c55c734387b
puglisac <alan.c.puglisi@gmail.com>
2021-02-25 13:55:12 -0600

    trying to add tests

--- test_app.py
+++ test_app.py
@@ -3,11 +3,12 @@ from unittest import TestCase
 from models import db, connect_db, User
 import os
 from app import app
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('TEST_DATABASE_URI')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = False
-app.config['SECRET_KEY'] = "some_secret_key"
 
-connect_db(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('TEST_DATABASE_URI')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_ECHO'] = False
+# app.config['SECRET_KEY'] = "some_secret_key"
+
+# connect_db(app)
 
 # db.create_all()
